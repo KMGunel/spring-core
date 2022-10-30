ce2d636ebad8cc4e119c71358aaf380f6e143c73
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-10-30 10:12:01 +0400

    spring-jpa-gunel created

---
+++ spring-jpa-gunel/src/main/java/az/developia/springjpagunel/SpringJpaGunelApplication.java
@@ -0,0 +1,13 @@
+package az.developia.springjpagunel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaGunelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaGunelApplication.class, args);
+	}
+
+}
---
+++ spring-jpa-gunel/src/test/java/az/developia/springjpagunel/SpringJpaGunelApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.springjpagunel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaGunelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
