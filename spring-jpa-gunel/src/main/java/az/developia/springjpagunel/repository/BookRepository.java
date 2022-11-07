4395b387fe0983b0b1699c0e7758cb53f399e8d3
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-11-07 16:15:20 +0400

    spring basics practice created

--- spring-jpa-gunel/src/main/java/az/developia/springjpagunel/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package az.developia.springjpagunel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import az.developia.springjpagunel.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-		
-	}
